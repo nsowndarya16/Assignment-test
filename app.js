f1430219f1aa0ad3421fc7b353359b053a07a3f2
nsowndarya16 <nsowndarya16@yahoo.com>
2018-05-27 14:53:07 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var MongoClient = require('mongodb').MongoClient;
-var url = 'mongodb://13.57.181.193:27017/assigndb';
+var url = 'mongodb://172.31.3.231:27017/assigndb';
 
 
 app.get('/hello', function(req, res){
